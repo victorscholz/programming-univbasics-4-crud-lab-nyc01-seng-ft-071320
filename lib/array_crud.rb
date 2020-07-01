e39d8ef24432144c54a6232601fd24595c2c5101
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-01 21:40:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  arr = ["coffee", "grinder", "milk", "cup"]
+  barista = ["coffee", "grinder", "milk", "cup"]
 end
 
 def add_element_to_end_of_array(array, element)
-  arr << "pitcher"
+  barista << "pitcher"
 end
 
 def add_element_to_start_of_array(array, element)
