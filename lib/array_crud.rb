d5b7962742a96e3fe3d668da1ab6e904d5fc36be
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-01 21:59:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  puts "#{create_an_array.push(arrays!)}" 
+  puts "#{create_an_array.push(barista)}" 
 end
 
 def add_element_to_start_of_array(array, element)
