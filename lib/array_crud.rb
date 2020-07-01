2f606a44a03eda52d78d0267fc8ef8a898807c32
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-01 22:12:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  x = array..count
+  x = array.count
+  array.
 end
 
 def update_element_from_index(array, index_number, element)
