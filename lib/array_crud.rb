2bc75b4de667f81856af023f31f846d68355c322
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-01 21:32:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   create_an_array << "arrays!"
 end
+create_an_array
 
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
