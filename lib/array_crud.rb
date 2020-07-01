1c0ee6b2c021a7432e52f921994e067c8f5aa5d7
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-01 21:41:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  barista << "pitcher"
+  create_an_array << "pitcher"
 end
 
 def add_element_to_start_of_array(array, element)
