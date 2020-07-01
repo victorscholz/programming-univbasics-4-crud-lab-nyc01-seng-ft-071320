8175e5f63026ce557fe713661058f2be900b3909
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-01 21:48:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "pitcher"
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.("arrays!")
+  create_an_array.("latte")
 end
 
 def remove_element_from_end_of_array(array)
